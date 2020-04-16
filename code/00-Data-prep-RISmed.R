94bcd1a33f55a746d96d5dfb5feff3a07b44bc9f
ZhanhaoSu <cedricsu@foxmail.com>
2020-04-16 13:06:02 +0800

    modify data prep codes

--- code/00-Data-prep-RISmed.R
+++ code/00-Data-prep-RISmed.R
@@ -11,22 +11,7 @@ query_summary <- EUtilsSummary(query, type = 'esearch', db = 'pubmed', retmax = 
 # Collect query data
 query_record <- EUtilsGet(query_summary)
 
-# Select variables of interest and create dataframe
-Record_data <- data.frame(
-        Title = query_record@ArticleTitle,
-        Abstract = query_record@AbstractText,
-        Journal = query_record@Title,
-        PMID = query_record@PMID,
-        Year = query_record@YearPubmed,
-        Country = query_record@Country
-    )
-
-# Change title and abstract column as character 
-Record_data$Title <- as.character(Record_data$Title)
-Record_data$Abstract <- as.character(Record_data$Abstract)
-
-# Output the dataframe as csv file for later use
-write.csv(Record_data, file = here("data", "record-data.csv"), row.names = F)
-
+# Export the query record 
+saveRDS(query_record, file = here("data", "query-record.rds"))
 
     
